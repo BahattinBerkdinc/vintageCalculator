02519db486d93eacf021235058651843a7cdea26
Bahattin Berkdinc <bberkdinc@gmail.com>
2023-08-31 03:38:38 +0300

    finished

--- src/calculator-main/Calculator.jsx
+++ src/calculator-main/Calculator.jsx
@@ -1,47 +1,118 @@
-import React from 'react'
-import "./calculator.scss"
+import React, { useState } from 'react';
+import "./calculator.scss";
 
 const Calculator = () => {
+  const [enteredNum, setEnteredNum] = useState('0'); 
+  const [prevNum, setPrevNum] = useState(''); 
+  const [operator, setOperator] = useState(''); 
+  const [result, setResult] = useState('');
+
+  const handleNumberClick = (number) => {
+    if (result !== '' || enteredNum === '0') {
+      setEnteredNum(number);
+      setResult('');
+    } else {
+      setEnteredNum(enteredNum + number);
+    }
+    const clickSound = new Audio('/clicksound.mp3'); 
+    clickSound.play(); 
+  };
+
+  const handleOperatorClick = (op) => {
+    if (prevNum !== '') {
+      calculateResult();
+    }
+    setPrevNum(enteredNum);
+    setEnteredNum('');
+    setOperator(op);
+    const clickSound = new Audio('/clicksound.mp3'); 
+    clickSound.play(); 
+  };
+
+  const calculateResult = () => {
+    const num1 = parseFloat(prevNum);
+    const num2 = parseFloat(enteredNum);
+
+    if (operator === '+') {
+      setResult((num1 + num2).toString());
+    } else if (operator === '-') {
+      setResult((num1 - num2).toString());
+    } else if (operator === '*') {
+      setResult((num1 * num2).toString());
+    } else if (operator === '/') {
+      setResult((num1 / num2).toString());
+    }
+
+    setPrevNum('');
+    setOperator('');
+  };
+
+  const handleEqualClick = () => {
+    if (prevNum !== '' && operator !== '') {
+      calculateResult();
+      setEnteredNum("0");
+    }
+    const clickSound = new Audio('/clicksound.mp3'); 
+    clickSound.play(); 
+  };
+
+  const handleClearClick = () => {
+    setEnteredNum('0');
+    setPrevNum('');
+    setOperator('');
+    setResult('');
+    const clickSound = new Audio('/clicksound.mp3'); 
+    clickSound.play(); 
+  };
+
   return (
-    <div className="calculator-box">
-      <div className="result-screen">
-        322332
-      </div>
-      <div className="buttons">
-        <div className="left">
-            <div>
-                <div className="btn">c</div>
-                <div className="btn">=</div>
-                <div className="btn">/</div>
-            </div>
-            <div>
-                <div className="btn">7</div>
-                <div className="btn">8</div>
-                <div className="btn">9</div>
-            </div>
-            <div>
-                <div className="btn">4</div>
-                <div className="btn">5</div>
-                <div className="btn">6</div>
-            </div>
-            <div>
-                <div className="btn">1</div>
-                <div className="btn">2</div>
-                <div className="btn">3</div>
-            </div>
-            <div>
-                <div className="btn">0</div>
-                <div className="btn">.</div>
-            </div>
+    <div className="calc-first-box">
+      <div className="calculator-box">
+        <div className="result-screen">
+          <span>
+            {prevNum}
+            {operator}
+            {enteredNum}
+          </span>
+          <span>= {result}</span>
         </div>
-        <div className="right">
-            <div className='btn'>*</div>
-            <div className='btn'>-</div>
-            <div className='btn'>+</div>
+        <div className="buttons">
+          <div className="left">
+            <div>
+              <div className="btn" onClick={handleClearClick}>c</div>
+              <div className="btn" onClick={handleEqualClick}>=</div>
+              <div className="btn" onClick={() => handleOperatorClick('/')}>/</div>
+            </div>
+            <div >
+              {[7, 8, 9].map(num => (
+                <div className="btn" key={num} onClick={() => handleNumberClick(num.toString())}>{num}</div>
+                
+              ))}
+            </div>
+            <div>
+              {[4, 5, 6].map(num => (
+                <div className="btn" key={num} onClick={() => handleNumberClick(num.toString())}>{num}</div>
+              ))}
+            </div>
+            <div>
+              {[1, 2, 3].map(num => (
+                <div className="btn" key={num} onClick={() => handleNumberClick(num.toString())}>{num}</div>
+              ))}
+            </div>
+            <div>
+              <div className="btn" onClick={() => handleNumberClick('0')}>0</div>
+              <div className="btn" onClick={() => handleNumberClick('.')}>.</div>
+            </div>
+          </div>
+          <div className="right">
+            {['*', '-', '+'].map(op => (
+              <div className='btn' key={op} onClick={() => handleOperatorClick(op)}>{op}</div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Calculator
+export default Calculator;
